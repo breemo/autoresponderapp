3dab1c94ec98cb8dbe55d785839c4887f5ef5bba
breemo <ibrahim.ali84@hotmail.com>
2025-11-12 13:46:51 +0200

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -3,3 +3,14 @@ import { redirect } from 'next/navigation';
 export default function Home() {
   redirect('/login'); // يرسل المستخدم مباشرة لصفحة تسجيل الدخول
 }
+
+
+//'use client';
+//import { useEffect } from 'react';
+//import { useRouter } from 'next/navigation';
+
+//export default function Home() {
+//  const router = useRouter();
+//  useEffect(() => { router.replace('/login'); }, [router]);
+//  return null;
+//}
