040b6e87101c97f2c8ad4b94682c9a054beb10b3
breemo <ibrahim.ali84@hotmail.com>
2025-11-13 09:51:18 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: './',        // مهم جداً على Vercel
+  build: {
+    outDir: 'dist',  // يتطابق مع إعداد Vercel
+  },
 })
