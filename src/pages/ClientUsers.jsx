e589e411bc847abb5a5d4a6db8d7bb0e4f1c76a4
breemo <ibrahim.ali84@hotmail.com>
2025-11-18 08:24:02 +0200

    Update ClientUsers.jsx

--- src/pages/ClientUsers.jsx
+++ src/pages/ClientUsers.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react"; 
 import { supabase } from "../lib/supabaseClient";
 import { useParams } from "react-router-dom";
 
