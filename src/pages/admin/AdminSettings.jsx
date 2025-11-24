cadf12e51380c2ce77b65a1e6e1709281bfa0f17
breemo <ibrahim.ali84@hotmail.com>
2025-11-24 12:53:01 +0200

    Update AdminSettings.jsx

--- src/pages/admin/AdminSettings.jsx
+++ src/pages/admin/AdminSettings.jsx
@@ -2,9 +2,9 @@ import AdminLayout from "../../layouts/AdminLayout";
 
 export default function AdminSettings() {
   return (
-    <AdminLayout>
+    <div>
       <h1 className="text-2xl font-bold mb-4">الإعدادات</h1>
       <p className="text-gray-500">إعدادات الحساب والأنظمة.</p>
-    </AdminLayout>
+    </div>
   );
 }
