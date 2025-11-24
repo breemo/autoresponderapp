c686be86831fa499d3bdbf3c1cb670ad1c540d32
breemo <ibrahim.ali84@hotmail.com>
2025-11-24 12:52:06 +0200

    Update AdminMessages.jsx

--- src/pages/admin/AdminMessages.jsx
+++ src/pages/admin/AdminMessages.jsx
@@ -2,9 +2,9 @@ import AdminLayout from "../../layouts/AdminLayout";
 
 export default function AdminMessages() {
   return (
-    <AdminLayout>
+    <div>
       <h1 className="text-2xl font-bold mb-4">الرسائل المرسلة</h1>
       <p className="text-gray-500">جميع الرسائل التي تم إرسالها.</p>
-    </AdminLayout>
+    </div>
   );
 }
