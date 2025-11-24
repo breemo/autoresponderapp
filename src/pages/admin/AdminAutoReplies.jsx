6cdefc3f96749b10532494f4f6a960f2b35abe28
breemo <ibrahim.ali84@hotmail.com>
2025-11-24 12:49:18 +0200

    Update AdminAutoReplies.jsx

--- src/pages/admin/AdminAutoReplies.jsx
+++ src/pages/admin/AdminAutoReplies.jsx
@@ -2,9 +2,9 @@ import AdminLayout from "../../layouts/AdminLayout";
 
 export default function AdminAutoReplies() {
   return (
-    <AdminLayout>
+    <div>
       <h1 className="text-2xl font-bold mb-4">الردود التلقائية</h1>
       <p className="text-gray-500">هنا يمكنك إنشاء وتعديل الردود التلقائية.</p>
-    </AdminLayout>
+    </div>
   );
 }
