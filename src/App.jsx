688aedcc62c2f01acb0f8decfd4679f99401b041
breemo <ibrahim.ali84@hotmail.com>
2025-11-30 11:26:03 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 
-import { AuthProvider, useAuth } from "./context/AuthContext.jsx";
+import { useAuth } from "./context/AuthContext.jsx";
 
 // Layouts
 import AdminLayout from "./layouts/AdminLayout.jsx";
