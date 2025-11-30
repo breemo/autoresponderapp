7c39074e53ebfb2685f43bbd3c9b26b28bd78d22
breemo <ibrahim.ali84@hotmail.com>
2025-11-30 13:42:26 +0200

    Refactor AuthContext import to include AuthProvider

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 
-import { useAuth } from "./context/AuthContext.jsx";
+import { AuthProvider, useAuth } from "./context/AuthContext.jsx";
 
 // Layouts
 import AdminLayout from "./layouts/AdminLayout.jsx";
