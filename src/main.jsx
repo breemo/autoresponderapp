e69040a0f81b394bf284b2e1e8cf441c70f96e95
breemo <ibrahim.ali84@hotmail.com>
2025-11-30 10:09:15 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,10 @@
-// src/main.jsx
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-// أهم شيء: استدعاء AuthProvider
-import { AuthProvider } from "./context/AuthContext.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthProvider>
-        <App />
-    </AuthProvider>
+      <App />
   </React.StrictMode>
-);
+)
